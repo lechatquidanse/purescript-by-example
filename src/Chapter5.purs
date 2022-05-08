f14b194238ff628aba3a7968cae0d51f0b03303d
stephane <stephane@zeelo.co>
2022-05-08 17:29:12 +0200

    :books: chapter 5, ADT

--- src/Chapter5.purs
+++ src/Chapter5.purs
@@ -1,12 +1,22 @@
 module Chapter5
-  ( factorial
+  ( Point(..)
+  , Shape(..)
+  , area
+  , circleOriginRadius10
+  , extractText
+  , factorial
   , fromSingleton
+  , origin
   , pascalRule
   , sameCity
+  , scaleBy2
   )
   where
 
 import Prelude
+
+import Data.Maybe (Maybe(..))
+import Data.Number (pi)
 
 -- Exercice 1
 factorial :: Int -> Int
@@ -29,3 +39,38 @@ sameCity { address: { city: x } } { address: { city: y } } = x == y
 fromSingleton :: forall a. a -> Array a -> a
 fromSingleton _ [x] = x
 fromSingleton x _ = x
+
+-- Exercice 5
+data Point = Point
+  { x :: Number
+  , y :: Number
+  }
+data Shape
+  = Circle Point Number
+  | Rectangle Point Number Number
+  | Line Point Point
+  | Text Point String
+
+origin :: Point
+origin = Point { x, y }
+  where
+    x = 0.0
+    y = 0.0
+-- @todo missing tests
+circleOriginRadius10 :: Shape
+circleOriginRadius10 = Circle origin 10.0
+-- @todo missing tests
+scaleBy2 :: Shape -> Shape
+scaleBy2 (Circle _ r) = Circle origin (r*2.0)
+scaleBy2 (Rectangle _ w h) = Rectangle origin (w*2.0) (h*2.0)
+scaleBy2 (Text _ s) = Text origin s
+scaleBy2 line = line
+-- @todo missing tests
+extractText :: Shape -> Maybe String
+extractText (Text _ string) = Just string
+extractText _ = Nothing
+-- @todo missing tests
+area :: Shape -> Number
+area (Circle _ r) = pi * r * r
+area (Rectangle _ w h) = w * h
+area _ = 0.0
